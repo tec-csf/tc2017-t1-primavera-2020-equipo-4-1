bd83ce0f394a806e914d4c96b74b669fb95fdbf0
Isaac Garza <isaacgarza@MacBook-Pro-de-Isaac.local>
2020-02-28 14:27:04 -0600

    Se metodifico de manera generica leer y guardar

--- sources/LeerGuardar.cpp
+++ sources/LeerGuardar.cpp
@@ -2,16 +2,58 @@
 C++ implementation to read from a file line by line
 and save the lines in an array.
 */
-
-#include <bits/stdc++.h> 
+#include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std; 
+
+template <class T> 
+class LeerArchivo{
+    public:
+        int i = 0;
+        int a = 0;
+        
+
+            //Input of the file for reading
+            void leerGuardar(T argv[]){
+                string str;
+                //Declaring an ifstream file and opening our file
+                ifstream file;
+                file.open(argv, ios::in);             
+
+                //Reading from the file line by line 
+                while (getline(file, str))
+                {
+                    i++;
+                }//Close while
+
+                //Closing the file
+                file.close();
+
+                file.open(argv, ios::in);
+                
+                string list[i];
+
+                while (getline(file, str))
+                {
+                list[a] = str; 
+                a++;
+                }//Close while 
+
+                //Printing
+                for (int s = 0; s < i; s++){
+                    cout << list[s] << endl;
+                }//Close for 
+
+                file.close();
+    
+            }
+};
   
 int main(int argc, char * argv[])
 {
-    //Declaring an ifstream file and opening our file
+  /*  //Declaring an ifstream file and opening our file
     ifstream file;
     file.open(argv[1], ios::in);
 
@@ -43,5 +85,9 @@ int main(int argc, char * argv[])
         cout << list[s] << endl;
     }//Close for 
 
-    file.close();
-}//Close main 
+    file.close();*/
+//Close main 
+    LeerArchivo<char> L;
+    L.leerGuardar(argv[1]);
+    return 0; 
+}
