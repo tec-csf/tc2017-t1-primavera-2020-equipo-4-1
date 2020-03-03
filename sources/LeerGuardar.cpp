5c55585f5e336b3c9fd28b639324393a2b66ea83
Isaac Garza <isaacgarza@MacBook-Pro-de-Isaac.local>
2020-03-03 11:03:52 -0600

    Declaracion algoritmo casi acabada y correcion de parametros para abrir archivo

--- sources/LeerGuardar.cpp
+++ sources/LeerGuardar.cpp
@@ -17,47 +17,53 @@ class LeerArchivo{
         int a = 0;
         
         //Input of the file for reading
-        void leerGuardar(T argv[]){
-            string str;
+        void leerGuardar(T argv1[], T argv2[]){
+            if (*argv1 == '-')
+                    {
+                      string path = argv2;
+                      string str;
+                      //Declaring an ifstream file and opening our file
+                      ifstream file;
+                      file.open(path, ios::in);             
 
-            //Declaring an ifstream file and opening our file
-            ifstream file;
-            file.open(argv, ios::in);             
+                      //Reading from the file line by line 
+                      while (getline(file, str))
+                      {
+                          i++;
+                      }//Close while
 
-            //Reading from the file line by line 
-            while (getline(file, str))
-            {
-                i++;
-            }//Close while
+                      //Closing the file
+                      file.close();
 
-            //Closing the file
-            file.close();
+                      //Opening the file again
+                      file.open(path, ios::in);
+                          
+                      string list[i];
 
-            //Opening the file again
-            file.open(argv, ios::in);
-                
-            string list[i];
+                      //Saving the text file line by line in an array
+                      while (getline(file, str))
+                      {
+                          list[a] = str; 
+                          a++;
+                      }//Close while 
 
-            //Saving the text file line by line in an array
-            while (getline(file, str))
-            {
-                list[a] = str; 
-                a++;
-            }//Close while 
+                      //Printing
+                      for (int s = 0; s < i; s++){
+                          cout << list[s] << endl;
+                      }//Close for 
 
-            //Printing
-            for (int s = 0; s < i; s++){
-                cout << list[s] << endl;
-            }//Close for 
-
-            //Closing the file again
-            file.close();
+                      //Closing the file again
+                      file.close();
+                      
+                    }else{
+                      cout<<"no funciona"<<endl;
+                    }
         }//Close leerGuardar
 };//Close leerArchivo
   
 int main(int argc, char * argv[])
 {
     LeerArchivo<char> L;
-    L.leerGuardar(argv[1]);
+    L.leerGuardar(argv[1],argv[argc-1]);
     return 0; 
 }//Close main
