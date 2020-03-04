cfa2137bdb5a7fd3417886fc5b234ce0905672af
Isaac Garza <isaacgarza@MacBook-Pro-de-Isaac.local>
2020-03-04 15:57:32 -0600

    FALTA ARREGLAR LA SEPARACION DE PALABRAS

--- sources/Main.cpp
+++ sources/Main.cpp
@@ -2,19 +2,70 @@
 C++ implementation to read from a file line by line
 and save the lines in an array.
 */
-
+#include <vector>
+#include <sstream>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <regex> 
-#include <sstream>
 using namespace std; 
 
 template <class T> 
 class Metodos{  
   public:
+      
+      string separate(string str) 
+            { 
+              string word = ""; 
+              for (auto x : str) 
+              { 
+                  if (x == ' ') 
+                  {
+                      word = ""; 
+                  } 
+                  else
+                  { 
+                      word = word + x; 
+                  } 
+                  return word; 
+              }  
+              return word; 
+            } 
   
-    
+
+    int declaracion(T *arr){
+          int n = sizeof(arr);
+          int contador = 0;
+          
+          //GUERO ARREGLA ESTO
+          for(int k = 0; k < n+1 ; k++){
+               cout<<separate(arr[0])<<endl;
+            }/*
+                for(int i = 0; i < splittedStrings.size() ; i++)
+                    { 
+                        // displaying content
+                        if(word.length()>1)
+                        {
+                            for(int i = 0; word.length()>i;i++)
+                            {
+                                if(word[i] == '+' && word[i+1] == '+')
+                                {
+                                    contador++;
+                                }
+                            }
+                        }
+                        else if (word == "=" || word == "<" || word == "+")
+                        {
+
+                            contador++;
+                        }
+                        cout << word << endl;
+                    } 
+                    cout<<"OE: "<<contador;
+                  }  
+                  return contador;
+          */
+         }
+
 };
 
 //Creating a template class
@@ -80,7 +131,9 @@ int main(int argc, char * argv[])
 {
     LeerArchivo<char> L;
     Metodos<string> M;
+
     string *arr = L.leerGuardar(argv[1],argv[argc-1]);
+    M.declaracion(arr);
 
     return 0; 
 }//Close main
