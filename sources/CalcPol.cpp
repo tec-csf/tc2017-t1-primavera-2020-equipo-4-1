f91cf55b9bdbb83110fbbcc685bccf0c1f2504eb
sergio-hernandez-castillo <a01025210@itesm.mx>
2020-03-07 18:31:00 -0600

    Ya reconoce "n" o "n+#/n-#" en un for.

--- sources/CalcPol.cpp
+++ sources/CalcPol.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 using namespace std; 
 
 /*ADAPTAR ESTE ALGORITMO AL MAIN E INCLUIR 
@@ -30,74 +31,99 @@ APROVECHEN QUE YA SE ORGANIZA SOLO PARA PONER EL EXPONENTE MAS ALTO AL PRINCIPIO
 
 */
 
-
+// Function to remove all spaces from a given string 
+string removeSpaces(string str)  
+{ 
+    str.erase(remove(str.begin(), str.end(), ' '), str.end()); 
+    return str; 
+}  
 
 void calc(string arr[2]){
-        string str2;
-        string str1;
-        int posIni;
-        int posTer;
-        int numIni;
-        string line;
-        int n = 2;
-        bool sw = false;
-        cout<<n<<endl;
-            for (int i = 0; i < n-1; i++)
-            {
-                    line = arr[i];
-                    cout<<line<<endl;
-                    stringstream file(line); 
-                    string word;
-                    while (file >> word){ 
+  string str2;
+  string str1;
+  string str3;
+  string signo;
+  int str3Int;
+  int posIni;
+  int posTer;
+  int numIni;
+  int numTer;
+  int suma;
+  string line;
+  int n = 2;
+  bool sw = false;
+  bool sumaResta = false;
 
-                    // Finds the line with the for
-                    if(word == "for"){
-                      //puts a position to the beggining and end of the for
-                      posIni = line.find("=")+2;
-                      posTer = posIni + 7;
-                      sw = true;
-                    }else{
+  cout<<n<<endl;
+  for (int i = 0; i < n; i++)
+  {
+    line = arr[i];
+    cout<<line<<endl;
+    stringstream file(line); 
+    string word;
+    while (file >> word){ 
+      // Finds the line with the for
+      if(word == "for"){
+        //puts a position to the beggining and end of the for
+        posIni = line.find("=")+2;
 
-                    }
-                  }
-                  if(sw == true){
-                      //We got the substring via the positions (0 and N)
-                       str2 = line.substr(posIni,1); 
-                       str1 = line.substr(posTer,1); 
-                      cout<<str2<<endl;
-                      cout<<str1<<endl;
+        if ((arr[i].at(posIni + 9) == '-') || (arr[i].at(posIni + 9) == '+')){
+          str3 = line.substr((posIni + 9), 3);
+          str3 = removeSpaces(str3);
+          sumaResta = true;
+          posTer = posIni + 7;
+        }
 
-                      numIni = stoi(str2); //stoi() converts from string to int
+        else {
+          posTer = posIni + 7;
+        }
 
-                      int suma = (- numIni + 1);
-                      // the conditions were made for the sign in the string
-                      if(suma>=0){
-                      string total = str1 + "+" + to_string(suma);
+        sw = true;
+      }
 
-                      cout<<total<<endl;
-                      }else{
-                        string total = str1 + "" + to_string(suma);
-                        cout<<total<<endl;
-                      }
+      if(sw == true){
+        //We got the substring via the positions (0 and N)
+        str2 = line.substr(posIni,1); 
+        str1 = line.substr(posTer,1);
+        cout<<str2<<endl;
+        cout<<str1<<endl;
 
-                  }
-               
+        numIni = stoi(str2); //stoi() converts from string to int
 
-            }
-        
+        if (sumaResta == true){
+          str3Int = stoi(str3);
+          suma = (- numIni + str3Int + 1);
+        }
+
+        else {
+          suma = (- numIni + 1); //This determines how much the for will repeat
+        }
+                
+        // the conditions were made for the sign in the string
+        if(suma>=0){
+        string total = str1 + "+" + to_string(suma);
+
+        cout<<total<<endl;
+        }else{
+          string total = str1 + "" + to_string(suma);
+          cout<<total<<endl;
+        }
+
+        sw = false;
+        sumaResta = false;
+      }
+    }
+  }      
 }
 
 int main()
 {
-    string linea1 = "for (int i = 0; i < n; i++){";
-    string linea2 = "int a = 0";
+  string linea1 = "for (int i = 0; i < n; i++){"; 
+  string linea2 = "int a = 0;";
 
-    string arr[2] = {linea1, linea2};
+  string arr[2] = {linea1, linea2};
 
+  calc(arr);
 
-    calc(arr);
-
-
-
-   return 0;
+  return 0;
 }
