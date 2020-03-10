baa57294c2b660e29130a26e5534c3a3490e317d
RodrigoSibaja <42215175+RodrigoSibaja@users.noreply.github.com>
2020-03-09 22:28:39 -0600

    Con tabla y editar

--- sources/Main2.cpp
+++ sources/Main2.cpp
@@ -492,7 +492,42 @@ class LeerArchivo{
             }//Close else 
 
             return list;
-        }//Close leerGuardar    
+        }//Close leerGuardar
+        string * editar(string *arr){
+            int counter = 0;
+            string str = " ";
+            int z = 0;
+            for (int u = 0; u < sizeof(arr); u++){
+                str = arr[u];
+                while(z < str.length()){
+                if (str[z] == ' '){
+                    counter++;
+                }//Close if 
+
+                else {
+                    break;
+                }//Close else 
+                z++;
+                }//Close for
+                str.erase(0, counter);
+                arr[u] = str;
+            }//Close for
+            return arr;
+        }//Close editar
+
+        void tabla(string *lineaCodigo){
+            string tabla[4][sizeof(lineaCodigo)];
+
+            cout<<"\nNo. de linea\tCódigo\t\t\t\t\t\t\tOE\tPolinomio"<<endl;;
+
+            for (int ty = 0; ty < sizeof(lineaCodigo); ty++)
+            {
+
+                    cout<<ty+1<<"\t\t"<<lineaCodigo[ty]<<"\t\t\t\t";
+                
+                cout<<endl;
+            }
+        }//Close editar
 };//Close leerArchivo
 
 int main(int argc, char * argv[])
@@ -510,6 +545,10 @@ int main(int argc, char * argv[])
     Conversion C;
 
     C.calculo(pol,i);
+
+    arr = L.editar(arr);
+
+    L.tabla(arr);
     
 
 
